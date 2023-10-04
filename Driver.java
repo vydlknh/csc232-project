9939361ee4aad55f7a60ec4bef9ffaf8ea511ea1
vydlknh <dolinhkhanhvy2004@gmail.com>
2023-10-03 20:57:10 -0400

    item class updated

--- Driver.java
+++ Driver.java
@@ -1,3 +1,6 @@
 public class Driver {
-  
+  public static void main(String[] args) {
+    Item newItem = new Item("Sandwich", "Food", "a peanut butter and jelly sandwich");
+    System.out.println(newItem.toString());
+  }
 }
